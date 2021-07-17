6bbe5666c7d45bfae584a1ef32468754d1311ea9
Harshit-svmx <hs.srivastava53@gmail.com>
2021-07-17 17:24:24 +0530

    Scripts added

---
+++ Assets/Scripts/UnityCycles.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityCycles : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
