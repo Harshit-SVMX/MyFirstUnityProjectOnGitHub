0ccce270cf0c2caef3daa45647c2658e3d24df2e
Harshit-svmx <hs.srivastava53@gmail.com>
2021-07-28 20:34:09 +0530

    committed, crouch functionality.

--- Assets/Scripts/Player_move.cs
+++ Assets/Scripts/Player_move.cs
@@ -2,17 +2,20 @@ using UnityEngine;
 
 public class Player_move: MonoBehaviour
 {
+    bool crouch_flag = false;
     public Animator animator;
+    public BoxCollider2D boxCollider2D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-         Player_movement();
+        Player_movement();
+        Player_crouch();
         //debugIt();
     }
 
@@ -36,5 +39,24 @@ public class Player_move: MonoBehaviour
         }
 
         transform.localScale = player_flipper;
+    }
+    public void Player_crouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            crouch_flag = !crouch_flag;                 // changing crouch flag true if false and false if true.
+        }
+        if (crouch_flag == true)
+        {
+            animator.SetBool("Crouch", crouch_flag);
+            //boxCollider2D.siz
+
+
+        }
+        else if (crouch_flag == false)
+        {
+            animator.SetBool("Crouch", crouch_flag);
+        }
+
     }
 }
